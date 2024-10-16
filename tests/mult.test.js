5235f667dd926a8f3decdd9ad0acee70cfb92c20
bataevvlad <bataevvlad@gmail.com>
2024-10-16 21:35:50 +0200

    ISSUE-1 correct tests and jest with hooks

--- tests/mult.test.js
+++ tests/mult.test.js
@@ -1,6 +1,6 @@
 const mult = require('../src/mult');
 
 it('should return correct value', () => {
-    const result = mult(2, 3);
-    expect(result).toBe(6)
+  const result = mult(2, 3);
+  expect(result).toBe(6);
 });
